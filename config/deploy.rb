8fd5b14352271257b56097aa821bb5707026eed0
Anthony <anthony_hill@bellsouth.net>
2015-03-22 21:42:40 -0400

    commiting config file

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+require 'bundler/capistrano'
+
+set :application, "flossfish"
+set :repository,  "git@github.com:github.com/bigant63/new_flossfish.git"
+set :deploy_to, "/var/www/flossfish.com"
+set :scm, :git
+set :branch, "master"
+set :user, "flossfis"
+set :scm_passphrase, "rhlg7cHlqWWqV"
+set :group, "deployers"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true, :port => 4321 }
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "50.23.201.238", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+=begin
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+=end
+
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
+end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
